b69c91828ea7688c3ea10533606f7ca1323a4b48
EdwinCastan <edcastanedacalde@s.sfusd.edu>
2026-01-30 12:00:57 -0800

    run and roll!!!

--- chemical-cleanse/character_body_2d.gd
+++ chemical-cleanse/character_body_2d.gd
@@ -8,13 +8,16 @@ const PLAYER_TEMP_RIGHT = preload("uid://c7fwbygop2vja")
 const PLAYER_TEMP = preload("uid://rkm5abtixqql")
 const PLAYER_TEMP_IDLE = preload("uid://2moox4unp1jl")
 const PLAYER_TEMP_ROLL = preload("uid://rognx6bw0y7o")
+const PLAYER_TEMP_RUN = preload("uid://bpdhx0w15yeb0")
 
-@export var speed = 600
-@export var roll_speed = 1000
+var normal_speed := 600
+var roll_speed := 1200
+var run_speed := 900
+var max_speed := normal_speed
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down",)
-	velocity = input_direction * speed
+	velocity = input_direction * max_speed
 
 func _physics_process(_delta):
 	get_input()
@@ -29,3 +32,16 @@ func _physics_process(_delta):
 	elif Input.is_action_pressed("down"):
 		fred.set_texture(PLAYER_TEMP_DOWN)
 		
+	if Input.is_action_pressed("run"):
+		fred.set_texture(PLAYER_TEMP_RUN)
+		max_speed = run_speed
+	if Input.is_action_just_released("run"):
+		max_speed = normal_speed
+		
+	if Input.is_action_just_pressed("roll"):
+		max_speed = roll_speed
+		get_node("timer").start()
+		fred.set_texture(PLAYER_TEMP_ROLL)
+
+func _on_timer_timeout() -> void:
+	max_speed = normal_speed
