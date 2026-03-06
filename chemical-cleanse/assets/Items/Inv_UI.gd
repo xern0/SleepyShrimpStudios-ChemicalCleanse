82be20ef57e9041f0afb57d1f6ee0e7404125f60
Beanofbean <jubarrioscastro@s.sfusd.edu>
2026-03-06 11:55:25 -0800

    half done on inventory

--- chemical-cleanse/assets/Items/Inv_UI.gd
+++ chemical-cleanse/assets/Items/Inv_UI.gd
@@ -1,9 +1,18 @@
 extends Control
 
+@onready var inv: Inv = preload("res://assets/Items/playerinv.tres")
+@onready var slots: Array = $NinePatchRect/GridContainer.get_children()
+
 var is_open = false
 
 func _ready():
+	update_slots()
 	close()
+
+
+func update_slots(): 
+	for u in range(min(inv.items.size(), slots.size())):
+		slots[u].update(inv.items[u])
 
 func _process(_delta):
 	if Input.is_action_just_pressed("inventory"):
