868a74f42f69c5dc34fa8682164a32de1d1174e0
Beanofbean <jubarrioscastro@s.sfusd.edu>
2026-03-20 12:07:34 -0700

    can see while press adn made a temp spray bopttoel

--- chemical-cleanse/inv_ui_slots.gd
+++ chemical-cleanse/inv_ui_slots.gd
@@ -20,7 +20,7 @@ func _gui_input(event: InputEvent):
 	if event is InputEventMouseButton:
 		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 			if inv.slots[my_index].item != null:
-				item_visual.visible = false
+				#item_visual.visible = false
 				is_dragging = false  
 		if !event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 			if !is_dragging:
@@ -29,7 +29,10 @@ func _gui_input(event: InputEvent):
 func _get_drag_data(at_position):
 	if inv.slots[my_index].item == null:
 		return null
-	is_dragging = true  
+	is_dragging = true
+	#only make the item insivisble while dragging
+	item_visual.visible = false
+
 	var preview = TextureRect.new()
 	preview.texture = inv.slots[my_index].item.texture
 	preview.size = Vector2(32, 32)
---
+++ chemical-cleanse/tempspraybottle.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+@onready var interactable: Area2D = $Interactable
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+func _ready() -> void:
+	interactable.Interact = _on_interact
+
+func _on_interact():
+	if sprite_2d.frame == 0:
+		sprite_2d.frame = 1
+		interactable.is_Interactable = false 
+		print("Gained a spray")
+		var inv = preload("res://assets/Items/playerinv.tres")
+		var item = preload("res://assets/Items/bottle.tres")  
+		inv.insert(item)
+		queue_free()
+
+var is_interactable := true
+var interact_name := "E"
