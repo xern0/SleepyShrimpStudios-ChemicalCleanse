d06e72e3695e1752b65f4aa2c50f9e2913ac61d2
Beanofbean <jubarrioscastro@s.sfusd.edu>
2026-04-17 01:37:24 -0700

    equip and hotbar done

--- chemical-cleanse/character_body_2d.gd
+++ chemical-cleanse/character_body_2d.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 @onready var fred: Sprite2D = $fred
 @onready var roll: Sprite2D = $roll
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var hand_sprite: Sprite2D = $fred/HandSprite
 
 @export var inv:Inv
 
@@ -18,21 +19,29 @@ var normal_speed := 600
 var roll_speed := 1200
 var run_speed := 900
 var max_speed := normal_speed
+var last_direction := Vector2.RIGHT
+var facing_right := true
 
 
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down",)
 	velocity = input_direction * max_speed
+	if input_direction != Vector2.ZERO:
+		last_direction = input_direction.normalized()
 
 func _physics_process(_delta):
+	_update_hand()
 	get_input()
+	_update_hand_layer()
 	move_and_slide()
 	fred.set_texture(PLAYER_TEMP_IDLE)
 	if Input.is_action_pressed("right"):
 		fred.set_texture(PLAYER_TEMP_RIGHT)
+		facing_right = true
 	elif Input.is_action_pressed("left"):
 		fred.set_texture(PLAYER_TEMP_LEFT)
+		facing_right = false
 	elif Input.is_action_pressed("up"):
 		fred.set_texture(PLAYER_TEMP)
 	elif Input.is_action_pressed("down"):
@@ -50,6 +59,7 @@ func _physics_process(_delta):
 		get_node("timer").start()
 		$fred.visible = false
 		$roll.visible = true
+		hand_sprite.visible = false
 
 
 
@@ -57,6 +67,11 @@ func _on_timer_timeout() -> void:
 	max_speed = normal_speed
 	$fred.visible = true
 	$roll.visible = false
+	max_speed = normal_speed
+	$fred.visible = true
+	$roll.visible = false
+	if Eventbus.current_item != null:  
+		hand_sprite.visible = true
 
 
 func _on_hurtbox_died() -> void:
@@ -68,3 +83,54 @@ func _on_hurtbox_hurt() -> void:
 
 func collect(item):
 	inv.insert(item)
+
+func _ready():
+	hand_sprite.visible = false
+	Eventbus.item_equipped.connect(_on_item_equipped)
+	hand_sprite.z_index = -1
+	print(hand_sprite.z_index)
+func _on_item_equipped(item):
+	if item == null:
+		hand_sprite.visible = false
+	else:
+		hand_sprite.visible = true
+		hand_sprite.texture = item.texture
+		hand_sprite.scale = Vector2(0.3, 0.3)
+		hand_sprite.position = Vector2(20, 0) 
+
+func _update_hand_position():
+	if !hand_sprite.visible:
+		return
+	var offset := Vector2.ZERO
+	if abs(last_direction.x) > abs(last_direction.y):
+		if last_direction.x > 0:
+			offset = Vector2(20, 0) 
+			hand_sprite.flip_h = false
+		else:
+			offset = Vector2(-20, 0) 
+			hand_sprite.flip_h = true
+	else:
+		if last_direction.y > 0:
+			offset = Vector2(0, 20) 
+		else:
+			offset = Vector2(0, -20)
+	hand_sprite.position = offset
+	_update_hand_position()
+
+func _update_hand():
+	if !hand_sprite.visible:
+		return
+	if facing_right:
+		hand_sprite.position = Vector2(20, 0)
+		hand_sprite.flip_h = false
+	else:
+		hand_sprite.position = Vector2(-20, 0)
+		hand_sprite.flip_h = true
+
+func _update_hand_layer():
+	if !hand_sprite.visible:
+		return
+	if last_direction.y < 0:  
+		fred.z_index = -1
+	elif last_direction.y > 0: 
+		fred.z_index = 1
