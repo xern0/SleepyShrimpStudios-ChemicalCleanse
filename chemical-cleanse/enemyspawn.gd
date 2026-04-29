79903abdce5768f9bead1a681f76adb0a240285d
Beanofbean <jubarrioscastro@s.sfusd.edu>
2026-04-28 22:01:04 -0700

    wokrs

--- chemical-cleanse/enemyspawn.gd
+++ chemical-cleanse/enemyspawn.gd
@@ -1,18 +1,22 @@
 extends Node2D
 
+var rand = RandomNumberGenerator.new()
+var enemy_scene = preload("res://enemyfix.tscn")
+var screen_size: Vector2
+var spawn_count = 5
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var rand = RandomNumberGenerator.new()
-	var enemy_scene = load("res://enemyfix.tscn")
-	var screen_size = get_viewport().get_visible_rect().size
-	
-	for i in range(0,10):
+	screen_size = get_viewport().get_visible_rect().size
+	$Timer.timeout.connect(_on_timer_timeout)
+	spawn_group() 
+
+func _on_timer_timeout() -> void:
+	spawn_group()
+
+func spawn_group() -> void:
+	for i in range(spawn_count):
 		var enemy = enemy_scene.instantiate()
-		rand.randomize()
-		var x = rand.randf_range(0,screen_size.x)
-		rand.randomize()
-		var y = rand.randf_range(0,screen_size.y)
-		enemy.position.y = y
-		enemy.position.x = x 
+		var x = rand.randf_range(0, screen_size.x)
+		var y = rand.randf_range(0, screen_size.y)
+		enemy.position = Vector2(x, y)
 		add_child(enemy)
