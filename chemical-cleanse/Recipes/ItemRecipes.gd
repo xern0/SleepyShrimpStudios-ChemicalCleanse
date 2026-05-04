f8292b8914051c66d59bdd3803dc5c12c1b4fc91
Beanofbean <jubarrioscastro@s.sfusd.edu>
2026-05-03 23:04:22 -0700

    half way done

---
+++ chemical-cleanse/Recipes/ItemRecipes.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name ItemRecipes
+
+
+@export var recipe_name : String = ""
+@export var recipe_final_name : InvItem = null
+@export var recipe_material_array : Array[InvItem] = []
